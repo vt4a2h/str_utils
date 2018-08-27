a838f47f8cc4a01bac1517d4ce34eee02437ff66
vt4a2h <vt4a2h@gmail.com>
2018-08-27 17:06:32 +0300

    Added documentation

--- src/split.hpp
+++ src/split.hpp
@@ -46,11 +46,25 @@ namespace str_utils {
     } // namespace detail
 
 
+    /**
+     * @brief Function for splitting std::string
+     * @note @p str should live long enough
+     * @param str input string
+     * @param sep separator
+     * @return vector of string views point to the parts of initial string
+     */
     auto split_str(const std::string &str, const std::string &sep = " ")
     {
         return detail::split_str_impl<char>({str.data(), str.size()}, sep);
     }
 
+    /**
+     * @brief Function for splitting std::wstring
+     * @note @p str should live long enough
+     * @param str input string
+     * @param sep separator
+     * @return vector of wstring views point to the parts of initial string
+     */
     auto split_wstr(const std::wstring &str, const std::wstring &sep = L" ")
     {
         return detail::split_str_impl<wchar_t>({str.data(), str.size()}, sep);
